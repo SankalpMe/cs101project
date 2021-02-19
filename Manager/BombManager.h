3f6cdb1ef3237b114981132ef026dd5d09ac6bc8
SankalpMe <bhamare.sankalp@gmail.com>
2021-02-19 19:23:38 +0530

    Deadly mode

--- Manager/BombManager.h
+++ Manager/BombManager.h
@@ -20,51 +20,58 @@ class BombManager {
     vector<BombInfo> bombs;
 
 public:
+    bool deadlyMode; // bombs respawn when set true
     BombManager() {
-
+        deadlyMode = false;
     }
 
-    //Add Coin To Game
+    //adds bomb to the game
     void addBomb(Vector2D position, Vector2D velocity = {0, -10}) {
-        Bomb *bomb = new Bomb(position, velocity, {0, COIN_G});
-        bomb->init();
+        Bomb *bomb = new Bomb(position, velocity, {0, COIN_G}); // similar physics as COIN
+        bomb->init(); // initialize  bomb ( Rendering and Constants ).
         bombs.push_back({bomb, 0});
+    } // end of addBomb(...)
 
-    }
-
+    // pauses all bombs in game
     void pauseAllBombs() {
         for (auto &bomb: bombs) {
             bomb.bomb->pause();
         }
-    }
+    } // end of :  pauseAllBombs()
 
+    // resumes the bombs
     void resumeAllBombs() {
         for (auto &bomb: bombs) {
             bomb.bomb->unpause();
         }
     }
 
-    //Handle game event steps for all bombs.
+    //Handle game steps for all bombs.
     void stepBombs(float timeStep, double currentTime) {
+
+        // to remove all destroyed / exploaded bombs from the scene
         vector<BombInfo> newbombs;
+        int bombsLost = 0;
         for(auto &bomb: bombs){
             if(!bomb.bomb->destroyed){
                 newbombs.push_back(bomb);
             }else{
                 delete bomb.bomb;
+                bombsLost++;
             }
         }
 
-        bombs = newbombs;
+        bombs = newbombs; // copies all the present bombs
 
-
+        // update each bomb
         for (auto &bomb: bombs) {
             bomb.bomb->nextStep(timeStep);
 
+            // remove bombs going out of scene
             if (bomb.bomb->getYPosition() > PLAY_Y_HEIGHT) {
                 bomb.bomb->reset();
                 bomb.bomb->destroyed = true;
-                bomb.bomb->hide();
+                bomb.bomb->hide(); // hide the bomb
                 bomb.endTime = currentTime;
 
             }
@@ -76,9 +83,18 @@ public:
             }
         }
 
+        // respawn bombs if deadly mode:
+
+        if(deadlyMode){
+            for(int i = 0 ; i < bombsLost;i++){
+                addBomb({0.0+rand() % WINDOW_X, PLAY_Y_HEIGHT},{0.0 + rand() % 50,-50.0- (rand()%100)}); // spawns a new bomb at different location
+            }
+        }
+
 
     }
 
+    // lasso collision handling
     void checkForLasso(Lasso &lasso) {
         for (auto &bomb: bombs) {
             lasso.check_for_bomb(bomb.bomb);
